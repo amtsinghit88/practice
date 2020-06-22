3685d598f237089902b4503b9c91f9e479c361e6
amit <amit.s@cleartrip.com>
2020-06-22 13:21:35 +0530

    changes

---
+++ src/test/java/DivisiblityOfString.java
@@ -0,0 +1,32 @@
+public class DivisiblityOfString {
+
+    public static void main(String [] args){
+
+        String s = "bcdbcdbcdbcd", t = "bcdbcd";
+        System.out.println(findSmallDivisor(s,t));
+    }
+
+    private static int  findSmallDivisor(String s, String t) {
+
+        if (s.length() % t.length() > 0) {
+            return -1;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i*t.length()<s.length();i++){
+            sb.append(t);
+        }
+
+        if(!sb.toString().equals(s))
+            return -1;
+        for(int i=1;i<=t.length();i++) {
+            sb = new StringBuilder();
+            String subStr = t.substring(0, i);
+            while(sb.length() < t.length()) {
+                sb.append(subStr);
+            }
+            if(sb.toString().equals(t))
+                return i;
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/MinimumAmount.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MinimumAmount {
+
+    public static void main(String [] args){
+        calculateAmount();
+
+    }
+
+    private static void calculateAmount() {
+        List<Integer> prices = new ArrayList<>();
+
+        prices.add(10);
+        prices.add(18);
+        prices.add(1);
+        prices.add(1);
+        prices.add(16);
+        prices.add(12);
+        prices.add(10);
+        prices.add(18);
+        prices.add(19);
+        prices.add(6);
+        prices.add(6);
+
+        long sum = prices.get(0);
+
+
+
+        for(int i =1;i<prices.size();i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            int min =0,fmin=1;
+            int j = 0;
+            while(j<i){
+
+                fmin = Math.min(prices.get(j),prices.get(j+1));
+                list.add(fmin);
+                j++;
+
+            }
+
+            min = Collections.min(list);
+            if(min>0){
+                min = prices.get(i)-min;
+                sum+=min;
+            }
+        }
+        System.out.println(sum);
+    }
+}
